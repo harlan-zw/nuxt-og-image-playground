642ff0e57409e1859229491175e79999ea8b1719
Harlan Wilton <harlan@harlanzw.com>
2023-05-31 22:24:48 +0800

    chore: netlify debug

--- server/api/resvg-len.png.ts
+++ server/api/resvg-len.png.ts
@@ -1,6 +1,5 @@
-import { defineEventHandler, setHeader } from 'h3'
+import { defineEventHandler, setHeader, sendStream } from 'h3'
 import { Resvg } from '@resvg/resvg-js'
-import sharp from 'sharp'
 
 export default defineEventHandler(async (e) => {
     const resvgJS = new Resvg('<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev/svgjs" viewBox="0 0 700 700" width="700" height="700"><defs><linearGradient gradientTransform="rotate(150, 0.5, 0.5)" x1="50%" y1="0%" x2="50%" y2="100%" id="ffflux-gradient"><stop stop-color="hsl(315, 100%, 72%)" stop-opacity="1" offset="0%"></stop><stop stop-color="hsl(227, 100%, 50%)" stop-opacity="1" offset="100%"></stop></linearGradient><filter id="ffflux-filter" x="-20%" y="-20%" width="140%" height="140%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" color-interpolation-filters="sRGB">\n' +
@@ -22,5 +21,5 @@ export default defineEventHandler(async (e) => {
     // add size header
     setHeader(e, 'Content-Length', png.length)
     // we return png back to a buffer
-    return png
+    return e.node.res.end(png);
 })
