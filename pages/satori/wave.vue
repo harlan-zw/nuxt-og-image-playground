e56a4a36280e4d4db1f0a0860f5d3f1441c304a7
harlan <harlan@harlanzw.com>
2025-01-02 13:08:59 +1100

    chore: test wave
    Relates to https://github.com/harlan-zw/nuxt-seo/issues/370

---
+++ pages/satori/wave.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+defineOgImageComponent('Wave', {
+  title: 'Wellness Waves',
+  description: 'Your Gentle Path to Health & Happiness',
+  fill: '#77BBAF',
+})
+</script>
+<template>
+<div>wave</div>
+</template>
